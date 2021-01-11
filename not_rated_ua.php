f54f1396d0896042fcae77949952340798630aa5
Denis Shagimuratov <poluxster@gmail.com>
2021-01-11 10:24:00 +0500

    Update not_rated_ua.php
    By user request https://github.com/CleanTalk/anti-ddos-lite/issues/11

--- not_rated_ua.php
+++ not_rated_ua.php
@@ -3,5 +3,15 @@
 $not_rated_ua = array(
     'CleanTalk Uptime bot.+'
 );
+$not_rated_ua = array(
+        'CleanTalk Uptime bot.+',
+        'Googlebot', 
+        'Bingbot',
+        'Baiduspider',
+        'YandexBot',
+        'facebot',
+        'facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)',
+        'ia_archiver'
+);
 
 ?>
