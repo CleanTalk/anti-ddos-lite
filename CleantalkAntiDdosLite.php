488855b714c1e85a7368bfaa84724bcacdfe8627
Glomberg <bazz@bk.ru>
2023-01-24 16:34:36 +0300

    Fix. Common. Class-wrapper added.

---
+++ CleantalkAntiDdosLite.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Cleantalk\CleantalkAntiDdosLite;
+
+class CleantalkAntiDdosLite
+{
+    public static function init()
+    {
+        require_once 'anti-ddos-lite.php';
+    }
+}
